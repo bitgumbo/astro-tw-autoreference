4ab7ac40e09c2dad256c14aa58c7c6a7a0cefc07
bitgumbo <bitgumbo@gmail.com>
2025-08-13 16:32:23 -0400

    chore: updated astro.config.mjs template to reflect option api

--- scripts/templates/astro.config.mjs
+++ scripts/templates/astro.config.mjs
@@ -2,13 +2,13 @@
 
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import twAutoReference from 'astro-tw-autoreference';
+import twAutoReference from 'astro-tw-autoreference/vite';
 
 export default defineConfig({
   vite: {
     plugins: [
       twAutoReference({
-        referencePath: '../styles/tailwind.css',
+        references: ['../styles/tailwind.css'],
       }),
       tailwindcss(),
     ],
